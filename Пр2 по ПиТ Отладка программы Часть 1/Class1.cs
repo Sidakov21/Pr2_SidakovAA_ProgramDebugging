7f4f9c1b53895df6a07506e8835dbde427e93687
Sidakov_Amir <121751618+Sidakov21@users.noreply.github.com>
2025-02-20 16:15:12 +0300

    Начаало работы с Буквами

--- Пр2 по ПиТ Отладка программы Часть 1/Class1.cs
+++ Пр2 по ПиТ Отладка программы Часть 1/Class1.cs
@@ -1,81 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Пр3_по_ПиТ_Отладка_программы_Часть_1
+class ArrayExample
 {
-    public class Galaxy
+    static void Main()
     {
-        public string Name { get; set; }
-
-        public double MegaLightYears { get; set; }
-        public GType GalaxyType { get; set; }
-
+        char[] letters = { 'f', 'r', 'e', 'd', ' ', 's', 'm', 'i', 't', 'h' };
+        string name = "";
+        int[] a = new int[10];
+        for (int i = 0; i < letters.Length; i++)
+        {
+            name += letters[i];
+            a[i] = i + 1;
+            SendMessage(name, a[i]);
+        }
+        Console.ReadKey();
     }
 
-    public class GType
+    static void SendMessage(string name, int msg)
     {
-        public object MyGType { get; set; }
-        private enum Type { Spiral, Elliptical, Irregular, Lenticular }
-
-        public GType(char type)
-        {
-            switch (type)
-            {
-                case 'S':
-                    MyGType = Type.Spiral;
-                    break;
-                case 'E':
-                    MyGType = Type.Elliptical;
-                    break;
-                case 'I':
-                    MyGType = Type.Irregular;
-                    break;
-                case 'L':
-                    MyGType = Type.Lenticular;
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
-
-    internal class Class1
-    {
-
-        private static void IterateThroughList()
-        {
-            var theGalaxies = new List<Galaxy>
-            {
-                new Galaxy() { Name="Tadpole", MegaLightYears=400, GalaxyType=new GType('S')},
-                new Galaxy() { Name="Pinwheel", MegaLightYears=25, GalaxyType=new GType('S')},
-                new Galaxy() { Name="Cartwheel", MegaLightYears=500, GalaxyType=new GType('L')},
-                new Galaxy() { Name="Small Magellanic Cloud", MegaLightYears=.2, GalaxyType=new GType('I')},
-                new Galaxy() { Name="Andromeda", MegaLightYears=3, GalaxyType=new GType('S')},
-                new Galaxy() { Name="Maffei 1", MegaLightYears=11, GalaxyType=new GType('E')}
-            };
-
-            foreach (Galaxy theGalaxy in theGalaxies)
-            {
-                Console.WriteLine(theGalaxy.Name + "  " + theGalaxy.MegaLightYears + ",  " + theGalaxy.GalaxyType.MyGType);
-            }
-
-            // Expected Output:
-            //  Tadpole  400,  Spiral
-            //  Pinwheel  25,  Spiral
-            //  Cartwheel, 500,  Lenticular
-            //  Small Magellanic Cloud .2,  Irregular
-            //  Andromeda  3,  Spiral
-            //  Maffei 1,  11,  Elliptical
-        }
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Welcome to Galaxy News!");
-            IterateThroughList();
-            Console.ReadKey();
-        }
+        Console.WriteLine("Hello, " + name + "! Count to " + msg);
     }
 }

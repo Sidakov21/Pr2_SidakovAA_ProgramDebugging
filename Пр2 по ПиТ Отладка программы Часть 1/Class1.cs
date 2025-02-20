30193a903dd7c09ea28d07cf4e7ad0a07c23d3a1
Sidakov_Amir <121751618+Sidakov21@users.noreply.github.com>
2025-02-20 13:41:22 +0300

    Создание новой ветки

--- Пр2 по ПиТ Отладка программы Часть 1/Class1.cs
+++ Пр2 по ПиТ Отладка программы Часть 1/Class1.cs
@@ -8,26 +8,6 @@ namespace Пр2_по_ПиТ_Отладка_программы_Часть_1
 {
     internal class Class1
     {
-        static int Fibonacci(int n)
-        {
-            int n1 = 0;
-            int n2 = 1;
-            int sum;
-
-            for (int i = 2; i <= n; i++)
-            {
-                sum = n1 + n2;
-                n1 = n2;
-                n2 = sum;
-            }
-
-            return n == 0 ? n1 : n2;
-        }
-
-        static void Main(string[] args)
-        {
-            int result = Fibonacci(5);
-            Console.WriteLine(result);
-        }
+        
     }
 }

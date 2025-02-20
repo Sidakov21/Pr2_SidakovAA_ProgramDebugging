3735224dade40bf14fa535f2933771c6e9b026f5
Sidakov_Amir <121751618+Sidakov21@users.noreply.github.com>
2025-02-20 11:30:39 +0300

    Добавьте файлы проекта.

---
+++ Пр2 по ПиТ Отладка программы Часть 1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Пр2_по_ПиТ_Отладка_программы_Часть_1
+{
+    internal class Class1
+    {
+        static int Fibonacci(int n)
+        {
+            int n1 = 0;
+            int n2 = 1;
+            int sum;
+
+            for (int i = 2; i < n; i++)
+            {
+                sum = n1 + n2;
+                n1 = n2;
+                n2 = sum;
+            }
+
+            return n == 0 ? n1 : n2;
+        }
+
+        static void Main(string[] args)
+        {
+            int result = Fibonacci(8);
+            Console.WriteLine(result);
+        }
+    }
+}
